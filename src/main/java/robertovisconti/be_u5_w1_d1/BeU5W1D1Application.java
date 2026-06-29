28381e22a6e9105f53ef481b9c07f67dad29743d
Roberto Visconti <robertovisconti93@gmail.com>
2026-06-29 12:34:16 +0200

    Initial commit

---
+++ src/main/java/robertovisconti/be_u5_w1_d1/BeU5W1D1Application.java
@@ -0,0 +1,13 @@
+package robertovisconti.be_u5_w1_d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU5W1D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU5W1D1Application.class, args);
+	}
+
+}
