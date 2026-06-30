6a9544a755031346d3740f5075ee22de4b974b93
Roberto Visconti <robertovisconti93@gmail.com>
2026-06-30 15:36:25 +0200

    last change

--- src/main/java/robertovisconti/be_u5_w1_d1/BeU5W1D1Application.java
+++ src/main/java/robertovisconti/be_u5_w1_d1/BeU5W1D1Application.java
@@ -2,17 +2,12 @@ package robertovisconti.be_u5_w1_d1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BeU5W1D1Application {
 
     public static void main(String[] args) {
-        ApplicationContext txc = SpringApplication.run(BeU5W1D1Application.class, args);
-
-//        Menu menu = txc.getBean(Menu.class);
-//
-//        menu.printMenu();
+        SpringApplication.run(BeU5W1D1Application.class, args);
     }
 
 }
