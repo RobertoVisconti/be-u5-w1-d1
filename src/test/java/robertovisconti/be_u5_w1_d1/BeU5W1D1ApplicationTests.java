658ebd20020634a9a09c4227939e5f09ef94cf9d
Roberto Visconti <robertovisconti93@gmail.com>
2026-07-01 17:26:57 +0200

    add other test

--- src/test/java/robertovisconti/be_u5_w1_d1/BeU5W1D1ApplicationTests.java
+++ src/test/java/robertovisconti/be_u5_w1_d1/BeU5W1D1ApplicationTests.java
@@ -5,11 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import robertovisconti.be_u5_w1_d1.entities.Menu;
-import robertovisconti.be_u5_w1_d1.entities.Ordine;
-import robertovisconti.be_u5_w1_d1.entities.Pizza;
-import robertovisconti.be_u5_w1_d1.entities.Tavolo;
+import robertovisconti.be_u5_w1_d1.entities.*;
 import robertovisconti.be_u5_w1_d1.enums.StatoTavolo;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,7 +19,9 @@ class BeU5W1D1ApplicationTests {
     private Menu menu;
 
     private Tavolo tavolo;
-    private Ordine ordine;
+
+    @Value("${pizzeria.costoCoperto}")
+    private double costoCoperto;
 
     @Test
     void contextLoads() {
@@ -59,7 +59,7 @@ class BeU5W1D1ApplicationTests {
     @Test
     @DisplayName("Test nuovo ordine associato al tavolo")
     public void testOrdine() {
-        this.ordine = new Ordine(2, tavolo, 2, 2.50);
+        Ordine ordine = new Ordine(2, tavolo, 2, costoCoperto);
         assertEquals(StatoTavolo.OCCUPATO, tavolo.getStatoTavolo());
     }
 
@@ -69,5 +69,25 @@ class BeU5W1D1ApplicationTests {
         tavolo = null;
     }
 
+
+    @Test
+    @DisplayName("Calcolo che l'importo totale del tavolo sia corretto")
+    public void testImportoTavolo() {
+        Ordine ordine = new Ordine(3, tavolo, 2, costoCoperto);
+
+        Pizza p1 = menu.getPizza().get(1);
+        Drink d1 = menu.getDrink().get(3);
+
+        ordine.addElement(p1);
+        ordine.addElement(d1);
+
+        double totale = ordine.getImportoTavolo();
+
+        double totProdotti = p1.getPrice() + d1.getPrice();
+        double totCoperti = 2 * costoCoperto;
+        double totOrdine = totCoperti + totProdotti;
+
+        assertEquals(totOrdine, totale, "Il calcolo del tavolo non è corretto");
+    }
 
 }
